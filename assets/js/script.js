94dafcd73054d578856e1a2384eeb3e2d5a2ca00
Musaib Ali <musaibshaheen@gmail.com>
2023-11-11 23:32:58 +0500

    anout and product-detail mobile version update

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,11 +50,11 @@ var mySwiper3 = new Swiper(".mySwiper3", {
         el: ".swiper-pagination",
         clickable: true,
         renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + '' + (index + 1) + "</span>";
+            return '<span class="' + className + '">' + '' + (index + 1) + "</span>";
         },
-      },
+    },
 
-   
+
     breakpoints: {
         420: {
             slidesPerView: 1,
@@ -62,19 +62,37 @@ var mySwiper3 = new Swiper(".mySwiper3", {
         },
 
         640: {
-            slidesPerView: 2,
+            slidesPerView: 1,
             spaceBetween: 6,
         },
         768: {
-            slidesPerView: 3,
+            slidesPerView: 2,
             spaceBetween: 10,
         },
         1024: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        1200: {
             slidesPerView: 4,
             spaceBetween: 10,
         },
     },
 });
+
+var produuctimgdetail = new Swiper(".produuct-imgdetail", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    autoplay: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    }
+});
+
+
+
 
 
 document.querySelector(".nav-item.dropdownmy").addEventListener("click", () => {
